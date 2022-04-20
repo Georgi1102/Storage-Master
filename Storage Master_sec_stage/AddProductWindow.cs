a0a7e9f58e694699bfbf484e21ad1f3e05da3635
George <44335333+Georgi1102@users.noreply.github.com>
2022-04-20 15:08:59 +0300

    Update AddProductWindow.cs

--- Storage Master_sec_stage/AddProductWindow.cs
+++ Storage Master_sec_stage/AddProductWindow.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Storage_Master_first_stage;
-using Storage_Master_first_stage.Entities;
-using Storage_Master_first_stage.Entities.Products;
 
 namespace Storage_Master_sec_stage
 {
+
     public partial class AddProductWindow : Form
     {
+
         public AddProductWindow()
         {
             InitializeComponent();
@@ -22,31 +17,46 @@ namespace Storage_Master_sec_stage
 
         private void addButton_Click(object sender, EventArgs e)
         {
-
-            StorageMaster st = new StorageMaster();
-           
-            string type = typeText.Text;
-            string price = priceText.Text;
-
-            double nonInputPrice = double.Parse(price);
-
-
-            string result = st.AddProduct(type, nonInputPrice);
-
-
-            if (CheckIfFormIsOpen("Form1"))
+            try
             {
-                var currForm = Application.OpenForms.Cast<Form>().FirstOrDefault(form => form.Name == "Form1");
+                StorageMaster st = new StorageMaster();
 
+                string type = typeText.Text;
+                string price = priceText.Text;
+              
+                double nonInputPrice = double.Parse(price);
+
+                string result = st.AddProduct(type, nonInputPrice);
+
+
+                if (CheckIfFormIsOpen("Form1"))
+                {
+                    var currForm = Application.OpenForms.Cast<Form>().FirstOrDefault(form => form.Name == "Form1");
+
+                    var mainWin = (Form1)currForm;
+
+                    mainWin.ControlPanel.Text = result;
+                    mainWin.CommandHistory.Items.Add(result);
+                    var products = st.Products;
+
+                    foreach (var product in products)
+                    {
+                        mainWin.AddedProducts.Items.Add(product.Key);
+                    }
+
+                    mainWin.Refresh();
+
+                    Close();
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+
+                //main window will always be Form1
+                var currForm = Application.OpenForms.Cast<Form>().FirstOrDefault(form => form.Name == "Form1");
                 var mainWin = (Form1)currForm;
 
-                
-
-                mainWin.ControlPanel.Text = result;
-                mainWin.CommandHistory.Items.Add(result);
-
-                mainWin.ControlPanel.AppendText(Environment.NewLine);
-                mainWin.Refresh();
+                mainWin.ControlPanel.Text = ex.Message;
 
                 Close();
             }
